b2b4aba49b972171a45ba9981e726e8885155981
Gyozmo <binantguillaume@gmail.com>
2018-06-06 21:41:33 +0200

    lodash +

--- nombre mistere/serv.js
+++ nombre mistere/serv.js
@@ -1,8 +1,14 @@
 var http = require('http');
+const _ = require('lodash');
+const _ = require('lodash/score');
+const fp = require('lodash/fp');
+
 const express = require('express');
 var app = express();
 app.get('/', function(req,res){
   res.render('accueil.ejs')
 });
+
+
 
 app.listen(8080);
