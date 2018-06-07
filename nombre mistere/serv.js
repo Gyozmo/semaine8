0005294973a8ac1320fb1c1afd682279a23ecaaf
Gyozmo <binantguillaume@gmail.com>
2018-06-07 10:14:44 +0200

    more moduls advancing in mistery number

--- nombre mistere/serv.js
+++ nombre mistere/serv.js
@@ -1,17 +1,65 @@
 var http = require('http');
 const _ = require('lodash');
-const _ = require('lodash/score');
+// const _ = require('lodash/score');
 const fp = require('lodash/fp');
 var array = require('lodash/array');
 var object = require('lodash/fp/object');
 var at = require('lodash/at');
 var curryN = require('lodash/fp/curryN');
 const express = require('express');
+var bodyParser = require('body-parser')
+
+
+let computerNumber = _.random(0, 10);;
+let storedRandom = computerNumber
+let countdown = 0;
+let hint = ""
+
 
 var app = express();
-app.get('/', function(req,res){
-  res.render('accueil.ejs')
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.get('/', function (req, res) {
+  res.render('accueil.ejs', {
+    userNumber: "",
+    storedRandom: "",
+    hint: "",
+    countdown: ""
+  })
 });
+
+app.post('/numberAction', function (req, res) {
+  
+
+  let userNumber = req.body.userNumber;
+ 
+
+  if (userNumber < storedRandom && countdown < 2) {
+    hint = "plus petit";
+    countdown++
+
+  } else if (userNumber > storedRandom && countdown < 2) {
+    hint = "plus grand";
+    countdown++
+  } else if (userNumber == storedRandom){
+    hint = "bravo";
+    
+  }
+
+
+
+  res.render('accueil.ejs', {
+    userNumber: userNumber,
+    storedRandom: storedRandom,
+    hint: hint,
+    countdown: countdown
+  })
+});
+
+
 
 
 
